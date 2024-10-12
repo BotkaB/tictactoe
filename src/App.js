12ae042abacb4549d1b37dfa83e2372bdcf1a575
BotkaB <botikabinaka@gmail.com>
2024-10-12 09:30:31 +0200

    kattintas buborékoltatva

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import Jatekter from './components/Jatekter';
 function App() {
 
   const lista = ["", "", "O", "", "X", "", "", "", "O"]
+
+  function katt(adat){
+    console.log("app-ban", adat)
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Tic-Tac-Toe</h1>
-        <article>
-          <div className="jatekter"></div>
-          <Jatekter lista={lista} />
-        </article>
       </header>
+      <article>
+        <div className="jatekter">
+        <Jatekter lista={lista} katt={katt}/>
+        </div>
+      </article>
+
       <footer>Botka Bianka</footer>
     </div>
   );
--- src/components/Jatekter.js
+++ src/components/Jatekter.js
@@ -3,14 +3,19 @@ import Cella from './Cella'
 
 export default function Jatekter(props) {
 
+    function katt(adat){
+        console.log("jatekterben", adat)
+        props.katt(adat)
+    }
+
     return (
-        <div>{
+        <>{
             props.lista.map((jel, index) => {
-                return <Cella jel={jel} key={index} />
+                return <Cella jel={jel} key={index} index={index} katt={katt}/>
             }
             )
             }
 
-        </div>
+        </>
     )
 }
